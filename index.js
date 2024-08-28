89d81a1cef2ae17c868e2ebf919ee1caf96fd0b6
Mnde81 <minubis@gmail.com>
2024-08-28 08:53:55 +0300

    updated file

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const port = 3000;
 
 
 
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    return res.status(500).send('Something broke!');
+});
+
 app.listen(port, () => {
     console.log(`Serveris pasileido: http://localhost:${port}`);
 });
